15c364a0c320fdb1ac81e187992521b92fb7798b
Jonathan Canela <jonathancanela365@gmail.com>
2020-04-30 17:07:10 -0700

    Finished

--- index.js
+++ index.js
@@ -41,7 +41,20 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood){
+    if(this.stomach.length >= 10){
+      return;
+    }else{
+      this.stomach.push(someFood);
+    }
+  }
+  poop(){ this.stomach =[];}
+  toString(){return this.name + ', ' + this.age;}
 }
 
 /*
@@ -59,7 +72,24 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){ this.tank += gallons}
+  drive(distance){ 
+    let driveableMiles = this.tank * this.milesPerGallon;
+    if(driveableMiles - distance <= 0){
+      this.tank = 0;
+      this.odometer += driveableMiles;
+      return 'I ran out of fuel at ' + this.odometer + ' miles!'
+    } else if(driveableMiles - distance > 0){ 
+      this.tank -= distance/this.milesPerGallon;
+      this.odometer += distance;
+    }
+  }
 }
 
 /*
@@ -75,7 +105,12 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(PerObj){
+    this.name = PerObj.name;
+    this.age = PerObj.age;
+    this.location = PerObj.location;
+  }
+  speak(){ return 'Hello my name is ' + this.name + ', I am from ' + this.location;}
 }
 
 /*
@@ -92,8 +127,16 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(Obj){
+    super(Obj);
+    this.specialty = Obj.specialty;
+    this.favLanguage = Obj.favLanguage;
+    this.catchPhrase = Obj.catchPhrase;
+  }
+  demo(subject){ return 'Today we are learning about ' + subject;}
+  grade(student, subject){ return student.name + ' receives a perfect score on ' + subject;}
+  grading(student){ student.grade += Math.random() * 10;}
 }
 
 /*
@@ -111,8 +154,29 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+  constructor(StuArgs){
+      super(StuArgs);
+      this.previousBackground = StuArgs.previousBackground;
+      this.className = StuArgs.className;
+      this.favSubjects = StuArgs.favSubjects;
+      this.grade = Math.random() * 100;    
+  }
+  listSubjects(){ 
+    let listOfSubjects = this.favSubjects.toString();
+    return 'Loving ' + listOfSubjects;
+  }
+  PRAssignment(subject){
+    return this.name + ' has submitted a PR for ' + subject;
+  }
+  sprintChallenge(subject){ return this.name + ' has begun spring challenge on ' + subject;}
+  graduate(){ 
+    if(this.grade >= 70){
+      return this.name + ' has met the requirements to graduate! Congrats you\'re now a Lambda School graduate!' 
+    }else{
+      return 'I\'m sorry, but you have not met the requirements to graduate.' 
+    }
+  }
 }
 
 /*
@@ -128,8 +192,25 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(PMObj){
+    super(PMObj);
+    this.gradClassName = PMObj.gradClassName;
+    this.favInstructor = PMObj.favInstructor;
+  }
+  standUp(channel){ return this.name + ' announces to ' + channel + ', @' + channel + ' standy times!';}
+  debugsCode(object, subject){ return this.name + ' debugs ' + object.name + '\'s code on ' + subject;}
+  grading(student){ 
+  function grading(){
+    let randomNumber = Math.random();
+    let randomGrade = Math.random();
+      if(randomNumber > 0.5){
+      student.grade+= Math.round(randomGrade) * 10;
+      }else if(randomNumber < 0.5){
+      student.grade -= Math.round(randomGrade) * 10;
+      }
+    }
+  }
 }
 
 /*
